5acd12ed4b74b8471e04e331823477202692345e
David Casper <davidcasper00@gmail.com>
2012-08-30 23:10:32 -0700

    Implemented android demo

--- src/com/dmcasper/yourtwocents/MainActivity.java
+++ src/com/dmcasper/yourtwocents/MainActivity.java
@@ -1,13 +1,25 @@
 package com.dmcasper.yourtwocents;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +31,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-
-    
 }
